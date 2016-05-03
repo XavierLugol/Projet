6cc07648affad0c841a75640940305e6a235d2af
xavier <xavier.lugol@laposte.net>
2016-05-03 10:33:36 +0200

    Commit deux

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,23 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
+=======
+		displayToto();
+		displayIiti();
+		displayIutu();
+	}
+	
+	private static void displayToto(){
+		System.out.println("toto");		
+	}
+	
+	private static void displayIiti(){
+		System.out.println("titi");		
+>>>>>>> Stashed changes
+	}
+	
+	private static void displayIutu(){
+		System.out.println("tutu");		
 	}
 }
