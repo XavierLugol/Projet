6f116172d1e9274b49494238d19a3bec0484dd1c
xavier <xavier.lugol@laposte.net>
2016-05-03 10:20:46 +0200

    INITIAL commit testing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-import java.util.Scanner;
+
 
 public class Main {
 	public static void main(String[] args) {
+		displayToto();
+	}
+	
+	private static void displayToto(){
+		System.out.print("toto");		
 	}
 }
